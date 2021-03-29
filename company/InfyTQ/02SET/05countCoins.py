4e5e3a3f7cc8dadf7073a368e86ad8bf85f1433a
vikas <v8kgsm@gmail.com>
2021-03-29 22:10:22 +0530

    count coins

--- company/InfyTQ/02SET/05countCoins.py
+++ company/InfyTQ/02SET/05countCoins.py
@@ -1,3 +1,10 @@
+# You have x no. of 5 rupee coins and y no. of 1 rupee coins.
+# You want to purchase an item for amount z.
+# The shopkeeper wants you to provide exact change.
+# You want to pay using minimum number of coins.
+# How many 5 rupee coins and i rupee coins will you use?
+# If exact change is not possible then display - 1.
+
 def make_amount(rupees_to_make, no_of_five, no_of_one):
     five_needed = int(rupees_to_make/5)
     one_needed = rupees_to_make % 5
