6c19026975d5612f8830f4126e7af14144acda66
vikas <v8kgsm@gmail.com>
2021-03-29 22:01:17 +0530

    play with 7

---
+++ company/InfyTQ/02SET/04play7.py
@@ -0,0 +1,19 @@
+def find_product(num1, num2, num3):
+    product = 0
+    # write your logic here
+    if(num1 == 7 or num2 == 7 or num3 == 7):
+        if(num1 == 7):
+            product = (num2*num3)
+        elif(num2 == 7):
+            product = (num3)
+        else:
+            product = (-1)
+    else:
+        product = (num1*num2*num3)
+
+    return product
+
+
+# Provide different values for num1, num2, num3 and test your program
+product = find_product(7, 6, 2)
+print(product)
