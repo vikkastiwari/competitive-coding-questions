febcf22b2901212d70623429fdcadae1e0701827
vikas <v8kgsm@gmail.com>
2021-04-09 19:59:11 +0530

    correctness

--- company/InfyTQ/Fundamentals/04SET/correctness.py
+++ company/InfyTQ/Fundamentals/04SET/correctness.py
@@ -1,5 +1,5 @@
 def find_correct(word_dict):
-    #start writing your code here
+    # start writing your code here
     answer = [0]*3
     for key, value in word_dict.items():
         if key == value:
@@ -11,15 +11,13 @@ def find_correct(word_dict):
             for index in range(len(key)):
                 if key[index] != value[index]:
                     c += 1
-            if c > 2:
+            if (c > 2):
                 answer[2] += 1
             else:
                 answer[1] += 1
     return answer
-        
-            
-        
-    
 
-word_dict={"THEIR": "THEIR","BUSINESS":"BISINESS","WINDOWS":"WINDMILL","WERE":"WEAR","SAMPLE":"SAMPLE"}
+
+word_dict = {"THEIR": "THEIR", "BUSINESS": "BISINESS",
+             "WINDOWS": "WINDMILL", "WERE": "WEAR", "SAMPLE": "SAMPLE"}
 print(find_correct(word_dict))
