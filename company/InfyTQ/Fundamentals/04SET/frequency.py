2287f72f07d943bd1893b0ecbb9c6929e64c7d6e
vikas <v8kgsm@gmail.com>
2021-04-12 21:49:01 +0530

    frequency

--- company/InfyTQ/Fundamentals/04SET/frequency.py
+++ company/InfyTQ/Fundamentals/04SET/frequency.py
@@ -1,19 +1,28 @@
 def max_frequency_word_counter(data):
     word = ""
     frequency = 0
-    l = []
-    l = data.split()
-    l1 = []
-    for value in l:
-        if value not in l1:
-            l1.append(value)
+    data = data.lower().split()
+    print(data)
+    for w in set(data):
+        c = data.count(w)
+        if c > frequency:
+            frequency = c
+            word = w
+        elif c == frequency and len(w) > len(word):
+            frequency = c
+            word = w
         else:
-            word = value
-            frequency += 1
+            pass
 
+    # start writing your code here
+    # Populate the variables: word and frequency
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
     print(word, frequency)
 
 
 # Provide different values for data and test your program.
-data = "Work like you do not need money, love like you have never been hurt, and dance like no one is watching"
+# data="Work like you do not need money, love like you have never been hurt, and dance like no one is watching"
+data = "Hands to clap and eyes to see"
 max_frequency_word_counter(data)
