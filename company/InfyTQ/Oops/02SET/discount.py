b5041c2bd998492410973922300cd1501b6f5f08
vikas <v8kgsm@gmail.com>
2021-04-14 11:14:55 +0530

    discount

--- company/InfyTQ/Oops/02SET/discount.py
+++ company/InfyTQ/Oops/02SET/discount.py
@@ -1,23 +1,22 @@
 class Mobile:
+    discount = 0
+
     def __init__(self, price, brand):
         self.price = price
         self.brand = brand
-        self.discount = 0
 
     def purchase(self):
-        total = self.price - self.price * self.discount / 100
+        total = self.price - self.price * Mobile.discount / 100
         print(self.brand, "mobile with price", self.price,
               "is available after discount at", total)
 
 
-def enable_discount(list_of_mobiles):
-    for mobile in list_of_mobiles:
-        mobile.discount = 50
+def enable_discount():
+    Mobile.discount = 50
 
 
-def disable_discount(list_of_mobiles):
-    for mobile in list_of_mobiles:
-        mobile.discount = 0
+def disable_discount():
+    Mobile.discount = 0
 
 
 mob1 = Mobile(20000, "Apple")
@@ -26,8 +25,8 @@ mob3 = Mobile(5000, "Samsung")
 mob4 = Mobile(6000, "Samsung")
 list_of_mobiles = [mob1, mob2, mob3, mob4]
 mob1.purchase()
-enable_discount(list_of_mobiles)
+enable_discount()
 mob2.purchase()
 mob3.purchase()
-disable_discount(list_of_mobiles)
+disable_discount()
 mob4.purchase()
