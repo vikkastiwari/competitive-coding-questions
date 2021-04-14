66a0eda2073d68018aee02cd4c11f41a7f8c980a
vikas <v8kgsm@gmail.com>
2021-04-14 20:14:33 +0530

    abstract

---
+++ company/InfyTQ/Oops/05SET/abstract.py
@@ -0,0 +1,27 @@
+from abc import ABCMeta, abstractmethod
+
+
+class Product(metaclass=ABCMeta):
+    @abstractmethod
+    def return_policy(self):
+        print("abstract method")
+
+
+# If a method is abstract, then the Subclass must override the abstract method.
+# Else we cannot instantiate the subclass also.
+class Furniture(Product):
+    def return_policy(self):
+        print("override abstract class")
+
+# If a child class overrides the abstract method,
+# then its own child classes need not override the abstract method.
+
+
+class Sofa(Furniture):
+    pass
+
+
+Sofa()
+
+f1 = Furniture()
+f1.return_policy()
