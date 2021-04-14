c11381afaad5d3c5cb44154f022ca92a3e65fcfa
Vikas Tiwari <51874681+vikkastiwari@users.noreply.github.com>
2021-04-14 11:20:16 +0530

    discount
    static variable
    static method

--- company/InfyTQ/Oops/02SET/discount.py
+++ company/InfyTQ/Oops/02SET/discount.py
@@ -1,32 +1,42 @@
 class Mobile:
-    discount = 0
-
+    __discount = 50
     def __init__(self, price, brand):
         self.price = price
         self.brand = brand
 
     def purchase(self):
-        total = self.price - self.price * Mobile.discount / 100
-        print(self.brand, "mobile with price", self.price,
-              "is available after discount at", total)
+        total = self.price - self.price * Mobile.__discount / 100
+        print (self.brand, "mobile with price", self.price, "is available after discount at", total)
 
+    @staticmethod
+    def enable_discount():
+        Mobile.set_discount(50)
 
-def enable_discount():
-    Mobile.discount = 50
+    @staticmethod
+    def disable_discount():
+        Mobile.set_discount(0)
 
+    @staticmethod
+    def get_discount():
+        return Mobile.__discount
 
-def disable_discount():
-    Mobile.discount = 0
+    @staticmethod
+    def set_discount(discount):
+        Mobile.__discount = discount
 
+mob1=Mobile(20000, "Apple")
+mob2=Mobile(30000, "Apple")
+mob3=Mobile(5000, "Samsung")
 
-mob1 = Mobile(20000, "Apple")
-mob2 = Mobile(30000, "Apple")
-mob3 = Mobile(5000, "Samsung")
-mob4 = Mobile(6000, "Samsung")
-list_of_mobiles = [mob1, mob2, mob3, mob4]
+Mobile.disable_discount()
+
 mob1.purchase()
-enable_discount()
+
+Mobile.enable_discount()
+
 mob2.purchase()
+
+Mobile.disable_discount()
+
 mob3.purchase()
-disable_discount()
-mob4.purchase()
+                                                    
