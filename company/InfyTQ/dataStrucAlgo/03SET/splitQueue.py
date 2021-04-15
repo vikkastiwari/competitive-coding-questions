8149d9911b04dcb1c5b4681fda301a92b24de8e0
vikas <v8kgsm@gmail.com>
2021-04-15 18:47:43 +0530

    .

--- company/InfyTQ/dataStrucAlgo/03SET/splitQueue.py
+++ company/InfyTQ/dataStrucAlgo/03SET/splitQueue.py
@@ -1,56 +1,56 @@
-  class Queue:
-       def __init__(self, max_size):
-            self.__max_size = max_size
-            self.__elements = [None]*self.__max_size
-            self.__rear = -1
-            self.__front = 0
+class Queue:
+    def __init__(self, max_size):
+        self.__max_size = max_size
+        self.__elements = [None]*self.__max_size
+        self.__rear = -1
+        self.__front = 0
 
-        def get_max_size(self):
-            return self.__max_size
+    def get_max_size(self):
+        return self.__max_size
 
-        def is_full(self):
-            if(self.__rear == self.__max_size-1):
-                return True
-            return False
+    def is_full(self):
+        if(self.__rear == self.__max_size-1):
+            return True
+        return False
 
-        def is_empty(self):
-            if(self.__front > self.__rear):
-                return True
-            return False
+    def is_empty(self):
+        if(self.__front > self.__rear):
+            return True
+        return False
 
-        def enqueue(self, data):
-            if(self.is_full()):
-                print("Queue is full!!!")
-            else:
-                self.__rear += 1
-                self.__elements[self.__rear] = data
+    def enqueue(self, data):
+        if(self.is_full()):
+            print("Queue is full!!!")
+        else:
+            self.__rear += 1
+            self.__elements[self.__rear] = data
 
-        def dequeue(self):
-            if(self.is_empty()):
-                print("Queue is empty!!!")
-            else:
-                data = self.__elements[self.__front]
-                self.__front += 1
-                return data
+    def dequeue(self):
+        if(self.is_empty()):
+            print("Queue is empty!!!")
+        else:
+            data = self.__elements[self.__front]
+            self.__front += 1
+            return data
 
-        def display(self):
-            for index in range(self.__front, self.__rear+1):
-                print(self.__elements[index])
+    def display(self):
+        for index in range(self.__front, self.__rear+1):
+            print(self.__elements[index])
 
-        # You can use the below __str__() to print the elements of the DS object while debugging
-        def __str__(self):
-            msg = []
-            index = self.__front
-            while(index <= self.__rear):
-                msg.append((str)(self.__elements[index]))
-                index += 1
-            msg = " ".join(msg)
-            msg = "Queue data(Front to Rear): "+msg
-            return msg
+    # You can use the below __str__() to print the elements of the DS object while debugging
+    def __str__(self):
+        msg = []
+        index = self.__front
+        while(index <= self.__rear):
+            msg.append((str)(self.__elements[index]))
+            index += 1
+        msg = " ".join(msg)
+        msg = "Queue data(Front to Rear): "+msg
+        return msg
 
 
 def split_queue(num_queue):
-      # Populate queue_list with odd_queue and even_queue
+    # Populate queue_list with odd_queue and even_queue
     queue_list = []
     # write your logic here
     odd_queue = []
