72666607029e46c31f2aa75629e64660f9189978
Vikas Tiwari <51874681+vikkastiwari@users.noreply.github.com>
2021-04-15 09:14:56 +0530

    merge

--- company/InfyTQ/dataStrucAlgo/01SET/merge.py
+++ company/InfyTQ/dataStrucAlgo/01SET/merge.py
@@ -1,7 +1,16 @@
 def merge_list(list1, list2):
-    merged_data = ""
+    merged_data = []
     # write your logic here
-    return resultant_data
+    for index1, l1 in enumerate(list1):
+        for index2, l2 in enumerate(list2[::-1]):
+            if(index1 == index2):
+                if(l1 == None):
+                    merged_data.append(l2)
+                elif(l2 == None):
+                    merged_data.append(l1)
+                else:
+                    merged_data.append(l1+l2)
+    return " ".join(merged_data)
 
 
 # Provide different values for the variables and test your program
