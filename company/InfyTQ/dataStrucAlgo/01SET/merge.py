c0dd40b842433b9d1f2d69e61cc33d1e3c27c255
vikas <v8kgsm@gmail.com>
2021-04-15 09:14:16 +0530

    merge

---
+++ company/InfyTQ/dataStrucAlgo/01SET/merge.py
@@ -0,0 +1,11 @@
+def merge_list(list1, list2):
+    merged_data = ""
+    # write your logic here
+    return resultant_data
+
+
+# Provide different values for the variables and test your program
+list1 = ['A', 'app', 'a', 'd', 'ke', 'th', 'doc', 'awa']
+list2 = ['y', 'tor', 'e', 'eps', 'ay', None, 'le', 'n']
+merged_data = merge_list(list1, list2)
+print(merged_data)
