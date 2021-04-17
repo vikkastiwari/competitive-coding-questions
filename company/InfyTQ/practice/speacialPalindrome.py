5a8299b967c4288b19807956008a35042839dd83
Vikas Tiwari <51874681+vikkastiwari@users.noreply.github.com>
2021-04-17 18:44:02 +0530

    Update speacialPalindrome.py

--- company/InfyTQ/practice/speacialPalindrome.py
+++ company/InfyTQ/practice/speacialPalindrome.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def special_palindrome(num):
     str_num = str(num)[::-1]
     sum_num = str(int(num)+int(str_num))
@@ -10,16 +9,4 @@ def special_palindrome(num):
 
 
 special_palindrome(195)
-=======
-def special_palindrome(num):
-    str_num = str(num)[::-1]
-    sum_num = str(int(num)+int(str_num))
 
-    if(sum_num == sum_num[::-1]):
-        print(sum_num)
-    else:
-        special_palindrome(sum_num)
-
-
-special_palindrome(195)
->>>>>>> ce81f1e9daeed4ad77a581aed91489021f8515d5
