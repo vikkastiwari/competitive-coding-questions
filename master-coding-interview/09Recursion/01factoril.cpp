dd7daa49a1f3dbcba452fd46dcb9cc0be5351fff
vikas <v8kgsm@gmail.com>
2021-05-13 10:46:15 +0530

    recursive factorial

---
+++ master-coding-interview/09Recursion/01factoril.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int n)
+{
+    if (n < 0)
+    {
+        return 0;
+    }
+    if (n == 0 || n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        // fact(5) = 5*4*3*2*1
+        return n * fact(n - 1);
+    }
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    int factorial = fact(num);
+    cout << num << ": " << factorial << endl;
+    return 0;
+}
