68a0108101fefdce2b010e643d3192906fd6d99f
vikas <v8kgsm@gmail.com>
2021-05-17 12:10:14 +0530

    selection sort

---
+++ master-coding-interview/10Sorting/02selection.cpp
@@ -0,0 +1,48 @@
+/* Selection Sort
+
+  Time Complexity = O(n^2)
+  Space Complexity = O(1)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *num1, int *num2)
+{
+    int temp = *num1;
+    *num1 = *num2;
+    *num2 = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+
+            // swap the minimum index element and first index element
+            if (minIndex != i)
+                swap(&arr[minIndex], &arr[i]);
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+    cout << "Sorted Array is: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << "  ";
+    }
+
+    return 0;
+}
