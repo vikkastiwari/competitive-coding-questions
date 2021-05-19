1a8150e362bb2b0403042302217d2f2f977dc946
vikas <v8kgsm@gmail.com>
2021-05-19 21:09:11 +0530

    tree traversal

---
+++ master-coding-interview/11Searching/03treeTraversal.cpp
@@ -0,0 +1,87 @@
+// Different tree traversals - inOrder, preOrder ,postOrder
+#include <iostream>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+/* print its nodes according to the
+"bottom-up" postorder traversal. */
+void printPostorder(struct Node *node)
+{
+    if (node == NULL)
+        return;
+
+    // first recur on left subtree
+    printPostorder(node->left);
+
+    // then recur on right subtree
+    printPostorder(node->right);
+
+    // now deal with the node
+    cout << node->data << " ";
+}
+
+/* print its nodes in inorder*/
+void printInorder(struct Node *node)
+{
+    if (node == NULL)
+        return;
+
+    /* first recur on left child */
+    printInorder(node->left);
+
+    /* then print the data of node */
+    cout << node->data << " ";
+
+    /* now recur on right child */
+    printInorder(node->right);
+}
+
+/* print its nodes in preorder*/
+void printPreorder(struct Node *node)
+{
+    if (node == NULL)
+        return;
+
+    /* first print data of node */
+    cout << node->data << " ";
+
+    /* then recur on left sutree */
+    printPreorder(node->left);
+
+    /* now recur on right subtree */
+    printPreorder(node->right);
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << "\nPreorder traversal of binary tree is \n";
+    printPreorder(root);
+
+    cout << "\nInorder traversal of binary tree is \n";
+    printInorder(root);
+
+    cout << "\nPostorder traversal of binary tree is \n";
+    printPostorder(root);
+
+    return 0;
+}
