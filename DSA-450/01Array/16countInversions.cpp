fbf1fd785cab960bd7c5a543460f0e330e70e52d
vikas <v8kgsm@gmail.com>
2021-06-02 10:00:22 +0530

    count inversion

--- DSA-450/01Array/16countInversions.cpp
+++ DSA-450/01Array/16countInversions.cpp
@@ -6,7 +6,33 @@ using namespace std;
 int merge(vector<int> arr, vector<int> temp, int low, int mid, int high)
 {
     int i, j, k, inv_count = 0;
-    i = low, j = mid, k = high;
+    i = low, j = mid, k = low;
+
+    while (i <= mid - 1 && j <= high)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+            inv_count += (mid - i);
+        }
+    }
+
+    while (i <= mid - 1)
+    {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= high)
+    {
+        temp[k++] = arr[j++];
+    }
+
+    for (i = low; i <= high; i++)
+        arr[i] = temp[i];
 
     return inv_count;
 }
@@ -14,13 +40,13 @@ int merge(vector<int> arr, vector<int> temp, int low, int mid, int high)
 int mergeSort(vector<int> arr, vector<int> temp, int low, int high)
 {
     int inv_count = 0;
-    while (low < high)
+    if (low < high)
     {
         int mid = (low + high) / 2;
         inv_count += mergeSort(arr, temp, low, mid);
         inv_count += mergeSort(arr, temp, mid + 1, high);
 
-        inv_count += merge(arr, temp, low, mid, high);
+        inv_count += merge(arr, temp, low, mid + 1, high);
     }
     return inv_count;
 }
@@ -30,6 +56,7 @@ int main()
     vector<int> arr = {1, 20, 6, 4, 5};
     vector<int> temp(arr.size());
     int low = 0, high = arr.size();
-    int inversion = mergeSort(arr, temp, low, high);
+    int inversion = mergeSort(arr, temp, low, high - 1);
+    cout << inversion << endl;
     return 0;
 }
---
+++ DSA-450/01Array/20rearrange.cpp
@@ -0,0 +1,53 @@
+// question link: https://www.geeksforgeeks.org/rearrange-array-alternating-positive-negative-items-o1-extra-space/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rearrange(int arr[], int n)
+{
+    int pos = 0, neg = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            neg++;
+        }
+        else
+        {
+            pos++;
+        }
+    }
+    vector<int> arr1(neg);
+    vector<int> arr2(pos);
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] >= 0)
+        {
+            arr2[i] = arr[i];
+        }
+        if (arr[i] < 0)
+        {
+            arr1[i] = arr[i];
+        }
+    }
+
+    for (int i = 0; i < arr1.size(); i++)
+    {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < arr2.size(); i++)
+    {
+        cout << arr2[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, -4, -1, 4};
+    int n = 5;
+    sort(arr, arr + n);
+    rearrange(arr, n);
+    return 0;
+}
