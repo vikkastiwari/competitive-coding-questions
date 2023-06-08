657e7c96bb9f816050a3805d66ddfab2eebea81c
vikas.t <vikas.t@pepperfry.com>
2023-06-08 17:45:28 +0530

    patterna day 4

--- challenge/1-Basics/patterns.cpp
+++ challenge/1-Basics/patterns.cpp
@@ -206,8 +206,80 @@ void pattern15(){
     }
 }
 
+void pattern16(){
+    int n = 5;
+    for(int i=0;i<n;i++){
+        char val = 'A';
+        for(int j=0;j<n-i-1;j++){
+            cout << " ";
+        }
+        for(int j=1;j<=2*i+1;j++){
+            cout << val;
+            if(j<=(2*i+1)/2) val++;
+            else val--;
+        }
+        cout << endl;
+    }
+}
+
+void pattern17(){
+    int n = 5;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout << "*";
+        }
+        for(int j=1;j<2*i+1;j++){
+            cout << " ";
+        }
+        for(int j=n-i-1;j>=0;j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<n-i;j++){
+            cout << "*";
+        }
+        for(int j=1;j<2*i+1;j++){
+            cout << " ";
+        }
+        for(int j=n-i-1;j>=0;j--){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+void pattern18(){
+    int n = 5;
+    for(int i=n-1;i>=0;i--){
+        for(int j=0;j<=n-i-1;j++){
+            cout << "*";
+        }
+        for(int j=1;j<2*i+1;j++){
+            cout << " ";
+        }
+        for(int j=0;j<=n-i-1;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<=n-i-1;j++){
+            cout << "*";
+        }
+        for(int j=1;j<2*i+1;j++){
+            cout << " ";
+        }
+        for(int j=0;j<=n-i-1;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {   
-    pattern15();
+    pattern18();
     return 0;
 }
