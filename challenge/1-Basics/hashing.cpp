9b570c1d0d814ddde91671f7677fa4fef5be2af2
vikas.t <vikas.t@pepperfry.com>
2023-06-12 21:12:37 +0530

    hashing frequency day 8

--- challenge/1-Basics/hashing.cpp
+++ challenge/1-Basics/hashing.cpp
@@ -9,6 +9,22 @@ void computeHash(int arr[], int n){
     }
 }
 
+void frequency(int arr[], int n){
+    unordered_map<int, int> map;
+    int max = 0;
+    int min = n;
+    for(int i=0;i<n;i++)
+        map[arr[i]]++;
+    
+    for(auto itr : map){
+        cout << itr.first << itr.second << endl;
+        if(itr.second > max) max = itr.second;
+        if(itr.second < min) min = itr.second;
+    }
+    cout << "max: " << max << endl;
+    cout << "min: " << min << endl;
+}
+
 int main(){
     int n;
     cin >> n;
@@ -24,5 +40,11 @@ int main(){
         cin >> num;
         cout << hashh[num] << endl;
     }
+
+    /*
+    * count min and max frequency
+    */
+    int arr[] = { 1, 1, 1, 1, 1, 2, 1, 1, 3, 4, 3 };
+    frequency(arr,sizeof(arr) / sizeof(arr[0]));
     return 0;
 }
---
+++ challenge/2-sorting/sorting-I/selection-sort.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int arr[], int n){
+    
+}
+
+int main(){
+
+    int arr[] = { 13,46,24,52,20,9 };
+    selectionSort(arr,sizeof(arr) / sizeof(arr[0]));
+    return 0;
+}
