28f149c9ffc39a6d568394396fd8ac6cdef40ffb
vikas.t <vikas.t@pepperfry.com>
2023-06-16 13:08:19 +0530

    recursive approach - day 12

---
+++ challenge/2-sorting/sorting-II/rec-bubble-sort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+*   Best TC: O(N)
+*   Avg & Worst TC: O(N^2)
+*/
+
+void recBubbleSort(int arr[], int start, int n){
+    if(n==1) return;
+    while(arr[start]>arr[start+1]){
+        swap(arr[start],arr[start+1]);
+        start++;
+    }
+    recBubbleSort(arr,0,n-1);   
+}
+
+int main(){
+    int arr[] = { 6,5,4,3,2,1 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    recBubbleSort(arr,0,n);
+    cout << "Recurssive Bubble Sort: ";
+    for(int i=0; i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
---
+++ challenge/2-sorting/sorting-II/rec-insertion-sort.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+*   Best TC: O(N)
+*   Avg & Worst TC: O(N^2)
+*/
+void insertionSort(int arr[],int currentId, int n){
+    
+    if(currentId > n-1) return;
+    int currentIndex = currentId;
+    // it runs until the value=arr[currentIndex] is at its correct position
+    // e.g. arr[currentIndex-1] < arr[currentIndex] < arr[currentIndex+1]
+    while(currentIndex > 0 && arr[currentIndex-1] > arr[currentIndex]){ // in case of sorted array it never runs - best case
+        swap(arr[currentIndex - 1], arr[currentIndex]);
+        currentIndex--;
+    }
+    insertionSort(arr,currentId+1,n);
+    
+}
+
+int main(){
+    int arr[] = { 13,46,24,52,20,9 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    insertionSort(arr,0,n);
+    cout << "Insertion Sort: ";
+    for(int i=0; i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
