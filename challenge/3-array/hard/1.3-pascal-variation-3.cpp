ab631313ef7ad23b10e82b51bfaea54b5b057675
vikas.t <vikas.t@pepperfry.com>
2023-08-01 22:48:57 +0530

    pascal print row - day 48

---
+++ challenge/3-array/hard/1.3-pascal-variation-3.cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst;
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto itr : ans) {
+        for (auto el : itr) {
+            cout << el << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
