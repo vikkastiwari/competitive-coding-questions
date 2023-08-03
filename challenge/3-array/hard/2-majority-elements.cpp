8d5d01bb91bdcaac72e712f7bd0e659f71201f48
vikas.t <vikas.t@pepperfry.com>
2023-08-03 14:25:50 +0530

    counter issue fix

--- challenge/3-array/hard/2-majority-elements.cpp
+++ challenge/3-array/hard/2-majority-elements.cpp
@@ -39,19 +39,24 @@ vector<int> majorityElementOptimal(vector<int> arr){
     
     vector<int> res;
     for(int i=0;i<arr.size();i++){
-        if(cnt1=0 && el2!=arr[i]){  
+        if(cnt1==0 && el2!=arr[i]){  
             cnt1++;
             el1 = arr[i];
-        }else if(cnt2=0 && el1!=arr[i]){
+        }else if(cnt2==0 && el1!=arr[i]){
             cnt2++;
             el2 = arr[i];
         }else if(arr[i] == el1){
             cnt1++;
         }else if(arr[i] == el2){
-            cnt2;
+            cnt2++;
         }else {
             cnt1--; cnt2--;
         }
+    }
+    cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < arr.size(); i++) {
+        if (arr[i] == el1) cnt1++;
+        if (arr[i] == el2) cnt2++;
     }
     if(cnt1 > arr.size()/3){
         res.push_back(el1);
@@ -66,7 +71,7 @@ vector<int> majorityElementOptimal(vector<int> arr){
 int main()
 {
     vector<int> arr = {11,33,33,11,33,11};
-    vector<int> res = majorityElementBetter(arr);
+    vector<int> res = majorityElementOptimal(arr);
     for(int i=0;i<res.size();i++){
         cout << res[i] << " ";
     }
