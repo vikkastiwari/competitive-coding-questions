7b68d74ff48086306189cfac2e29d4e84a8ff180
vikas.t <vikas.t@pepperfry.com>
2023-08-05 13:45:49 +0530

    3 sum three pointer approach - day 52

--- challenge/3-array/hard/3-three-sum.cpp
+++ challenge/3-array/hard/3-three-sum.cpp
@@ -32,7 +32,7 @@ vector<vector<int>> threeSumBrute(vector<int> arr){
 
 /**
  * @brief threeSumBetter
- * TC: O(N2 * log(no. of unique triplets)), where N = size of the array
+ * TC: O(N^2 * log(no. of unique triplets)), where N = size of the array
  * SC: O(2 * no. of the unique triplets) + O(N) as we are using a set data structure 
  * and a list to store the triplets and extra O(N) for storing the array elements in another set.
  * 
@@ -59,10 +59,46 @@ vector<vector<int>> threeSumBetter(vector<int> arr){
     return res;
 }
 
+/**
+ * @brief threeSumOptimal
+ * TC: O(N^2 + log(no. of unique triplets)), where N = size of the array
+ * SC: O(no. of the unique triplets) 
+ */
+vector<vector<int>> threeSumOptimal(vector<int> arr){
+    int n = arr.size();
+    int target = 0;
+    int i = 0; // ith pointer will be fixed will only move j and k
+    sort(arr.begin(), arr.end());
+    vector<vector<int>> res;
+    for (int i = 0; i < n; i++) {
+        //remove duplicates:
+        if (i != 0 && arr[i] == arr[i - 1]) continue;
+        int j = i+1, k = n-1;
+        while(j<k ){
+            int sum = arr[i] + arr[j] + arr[k];
+            if(sum == target){
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                res.push_back(temp);
+                j++;k--;
+                
+                //  To skip the duplicate elements
+                while(j<k && arr[j]==arr[j-1]) j++;
+                while(j<k && arr[k]==arr[k+1]) k--;
+
+            }else if(sum > target) {
+                k--;
+            }else { // sum < target
+                j++;
+            }
+        }
+    }
+    return res;
+}
+
 int main()
 {
     vector<int> arr = {-1,0,1,2,-1,-4};
-    vector<vector<int>> res = threeSumBetter(arr);
+    vector<vector<int>> res = threeSumOptimal(arr);
     for(int i=0;i<res.size();i++){
         for(int j=0;j<res[i].size();j++){
         cout << res[i][j] << " ";
