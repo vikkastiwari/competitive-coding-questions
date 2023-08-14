46e244b67982f3f7a158a1e5171a499ac8825a6c
vikas.t <vikas.t@pepperfry.com>
2023-08-14 14:29:33 +0530

    merge sorted array - gap method - day 59

--- challenge/3-array/hard/8-merge-sorted-array.cpp
+++ challenge/3-array/hard/8-merge-sorted-array.cpp
@@ -38,6 +38,13 @@ void mergeSortedArray(vector<int> &arr1,vector<int> &arr2){
     }
 }   
 
+/**
+ * @brief Optimal 1 - two pointer method
+ * 
+ * @param arr1 
+ * @param arr2 
+ * @return * void 
+ */
 void mergeSortedArrayOptimal(vector<int> &arr1,vector<int> &arr2){
     int pt1=arr1.size()-1, pt2=0;
 
@@ -54,7 +61,8 @@ void mergeSortedArrayOptimal(vector<int> &arr1,vector<int> &arr2){
 }
 
 /**
- * @brief Optimised approach
+ * @brief Optimised approach - two pointer method
+ * push array 2 into array 1 and then sort it
  * 
  * @param arr1 
  * @param m 
@@ -76,12 +84,62 @@ void merge(vector<int>& arr1, int m, vector<int>& arr2, int n) {
     }
 }
 
+/**
+ * @brief optimal approach using gap method - internally it uses shell sort
+ * 
+ * @return * int 
+ */
+
+void swapIfGreater(vector<int> &arr1, vector<int> &arr2, int ind1, int ind2) {
+    if (arr1[ind1] > arr2[ind2]) {
+        swap(arr1[ind1], arr2[ind2]);
+    }
+}
+
+void mergeSortedAr(vector<int> &arr1, vector<int> &arr2, int n, int m) {
+    // len of the imaginary single array:
+    int len = n + m;
+
+    // Initial gap: 
+    // + (len % 2) convert's it into ceil
+    int gap = (len / 2) + (len % 2);
+
+    while (gap > 0) {
+        // Place 2 pointers:
+        int left = 0;
+        int right = left + gap;
+        while (right < len) {
+            // case 1: left in arr1[]
+            //and right in arr2[]:
+            if (left < n && right >= n) {
+                swapIfGreater(arr1, arr2, left, right - n);
+            }
+            // case 2: both pointers in arr2[]:
+            else if (left >= n) {
+                swapIfGreater(arr2, arr2, left - n, right - n);
+            }
+            // case 3: both pointers in arr1[]:
+            else {
+                swapIfGreater(arr1, arr1, left, right);
+            }
+            left++, right++;
+        }
+        // break if iteration gap=1 is completed:
+        if (gap == 1) break;
+
+        // Otherwise, calculate new gap:
+        gap = (gap / 2) + (gap % 2);
+    }
+}
+
+
 int main()
 {
     vector<int> arr1 = {1, 4, 8, 10};
     vector<int> arr2 = {2, 3, 9};
 
-    mergeSortedArray(arr1, arr2);
+    mergeSortedAr(arr1, arr2, arr1.size(), arr2.size());
+
     for(int i=0;i<arr1.size();i++){
         cout << arr1[i] << " ";
     }
