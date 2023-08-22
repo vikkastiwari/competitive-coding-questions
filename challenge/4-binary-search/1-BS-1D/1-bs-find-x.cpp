d76b7de7969814b3834cd028300593caa0e3b69b
vikas.t <vikas.t@pepperfry.com>
2023-08-22 09:21:30 +0530

    binary search recurssion - day 65

---
+++ challenge/4-binary-search/1-BS-1D/1-bs-find-x.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int> arr,int low, int high, int target){
+    if(low>high) return -1;
+
+    int mid = (low+high)/2;
+    if(arr[mid] == target) return mid;
+    else if(arr[mid]>target) 
+        return binarySearch(arr,low,mid-1,target);
+    return binarySearch(arr,mid+1,high,target);
+}
+
+/**
+ * @brief binary search recursive approach
+ * TC: O(logn) - if we get sorted array
+ * SC: O(1)
+ * 
+ * @param arr 
+ * @param target 
+ * @return * void 
+ */
+void findTarget(vector<int> arr, int target){
+    sort(arr.begin(),arr.end());
+    cout << binarySearch(arr,0,arr.size()-1,target) << endl;
+}
+
+int main(){
+    vector<int> arr = { 3, 4, 6, 7, 9, 12, 16, 17 };
+    int target = 31;
+    findTarget(arr,target);
+    return 0;
+}
