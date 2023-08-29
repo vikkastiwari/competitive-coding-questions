50d4d5042859bb29ee59e5866a3590a1be226722
vikas.t <vikas.t@pepperfry.com>
2023-08-29 11:40:28 +0530

    search element in sorted rotated array logN - day 71

--- challenge/4-binary-search/1-BS-1D/9-search-element-in-a-rotated-sorted-array-II.cpp
+++ challenge/4-binary-search/1-BS-1D/9-search-element-in-a-rotated-sorted-array-II.cpp
@@ -1,54 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-/**
- * @brief binary search approach
- * TC: O(logn)
- * SC: O(1)
- * 
- * @param arr 
- * @param target 
- * @return * int 
- */
-int bs(vector<int> arr, int low, int high, int target){
-    int occurance = -1;
-    while(low <= high){
-        int mid = (low+high)/2;
-        if(arr[mid]==target){
-            occurance = mid;
-            high = mid - 1;
-        }else if(arr[mid]>target){
-            high = mid-1;
-        }else{
-            low = mid + 1;
-        }
-    }
-    return occurance;
-}
-
-/**
- * @brief search element
- * TC: O(2logn)
- * SC: O(1)
- * 
- * @param arr 
- * @param target 
- * @return * int 
- */
-int searchElement(vector<int> arr, int target){
-    int breakPoint = 0;
-    if(arr[breakPoint] == target) return breakPoint;
-
-    for(int i=1;i<arr.size();i++){
-        if(arr[i-1]>arr[i]) breakPoint = i;
-    }
-    if(breakPoint != -1){
-        int part1 = bs(arr, 0, breakPoint-1, target);
-        int part2 = bs(arr, breakPoint, arr.size()-1, target);
-        return part1 != -1 ? part1 : part2;
-    }
-    return bs(arr, 0, arr.size()-1, target);
-}
+// Now duplicate elements will be present in the array
 
 /**
  * @brief search element more optimized
@@ -64,6 +17,11 @@ int searchElement2(vector<int> arr, int target){
     while(low <= high){
         int mid = (low+high)/2;
         if(arr[mid]==target) return mid;
+        if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+            low += 1;
+            high -= 1;
+            continue;
+        }
 
         // if left part is sorted
         if(arr[low] <= arr[mid]){
@@ -89,8 +47,8 @@ int searchElement2(vector<int> arr, int target){
 }
 
 int main(){
-    vector<int> arr = { 7, 8, 1, 2, 3, 4, 5, 6 };
-    int target = 0;
+    vector<int> arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6 };
+    int target = 3;
     cout << searchElement2(arr, target) << endl;
     return 0;
 }
