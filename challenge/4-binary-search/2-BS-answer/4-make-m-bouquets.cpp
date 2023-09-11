7ea9eb6fbd12bd73c281fc670d8da287f4b6e88c
vikas.t <vikas.t@pepperfry.com>
2023-09-11 12:15:18 +0530

    make m bouquets

--- challenge/4-binary-search/2-BS-answer/4-make-m-bouquets.cpp
+++ challenge/4-binary-search/2-BS-answer/4-make-m-bouquets.cpp
@@ -21,7 +21,7 @@ int findMin(vector<int> &arr) {
     return minEl;
 }
 
-int possible(vector<int> arr, int day, int m, int k){
+bool possible(vector<int> arr, int day, int m, int k){
     int cnt = 0;
     int countOfBouquets = 0;
     for(int i=0;i<arr.size();i++){
@@ -50,21 +50,52 @@ int possible(vector<int> arr, int day, int m, int k){
  * @return * int 
  */
 int makeMBouquets(vector<int> arr, int m, int k) {
-    if(arr.size() - 1 < m*k) return -1;
+    if(arr.size() < m*k) return -1;
 
     int maxDays = findMax(arr);
     int minDays = findMin(arr);
     for(int i=minDays;i<=maxDays;i++){
         int found = possible(arr,i,m,k);
-        if(found) return i;
+        if(found) return arr[i];
     }
     return -1;
 }
 
+/**
+ * @brief binary search approach where we iterate from minDays to maxDays
+ * TC: O(logn)
+ * SC:O(1)
+ * 
+ * @param arr 
+ * @param m 
+ * @param k 
+ * @return * int 
+ */
+int makeMBouquets2(vector<int> arr, int m, int k) {
+    long long val = m * 1ll * k * 1ll;
+    if(arr.size() < val) return -1;
+
+    int maxDays = findMax(arr);
+    int minDays = findMin(arr);
+    int low = minDays, high = maxDays;
+    while(low<=high){
+        int mid = (low+high) / 2;
+        int found = possible(arr,mid,m,k);
+        if(found){ // when found we will move to left of array in search of min possible days
+            high = mid - 1;
+        }else{ // when not found we will move to right in search of possibilities where it is possible to make m bouquets 
+            low = mid + 1;
+        }
+    }
+    return low;
+}
+
 int main(){
-    vector<int> arr = {7, 7, 7, 7, 13, 11, 12, 7}; // rose will bloom on the ‘arr[i]th’ day
-    int m = 2, k = 3; // m - number of bouquets, k - number of roses in each bouquet
+    // vector<int> arr = {7, 7, 7, 7, 13, 11, 12, 7}; // rose will bloom on the ‘arr[i]th’ day
+    // int m = 2, k = 3; // m - number of bouquets, k - number of roses in each bouquet
+    vector<int> arr = {  97, 83 }; // rose will bloom on the ‘arr[i]th’ day
+    int m = 2, k = 1; // m - number of bouquets, k - number of roses in each bouquet
     int ans = makeMBouquets(arr, m, k);
-    cout << ans << endl;
+    cout << arr[ans] << endl;
     return 0;
 }
